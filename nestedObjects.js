c30bd33b3567678fa624cdaa6304d4176b4672ec
saadeyoola <102014367+saadeyoola@users.noreply.github.com>
2022-03-21 13:37:00 +0000

    Create nestedObjects.js

---
+++ nestedObjects.js
@@ -0,0 +1,9 @@
+const _ = require('lodash')
+
+var object = {"a": { "b": { "c": "d" }}};
+
+ console.log("Result at 'a.b.c': ", _.get(object, 'a.b.c'));
+ console.log("Result at 'a.b.nonexistent key': ",_.get(object, 'a.b.nonexistent', "default result"));
+
+var object2 = {"x":{"y":{"z":"a"}}};
+console.log("Result at 'x.y.z': ",_.get(object2, 'x.y.z'));
